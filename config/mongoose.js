c3910c0d2141b3eb1336cfe3aec18097ce994e99
Kasim Saifi <106295428+Qasimsaifi@users.noreply.github.com>
2024-05-14 11:48:14 +0530

    mongo url changed

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost/Suvira");
+mongoose.connect("mongodb+srv://root:root@apidb.yrzqpy5.mongodb.net/node-api?retryWrites=true&w=majority");
 // mongoose.connect(process.env.MONGO_DB_ATLAS_STRING, { useNewUrlParser: true });
 
 const db = mongoose.connection;
